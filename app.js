2f33ef9f8aa093ed6ab3c24c95802e20ef01d07d
HarshwardhanPatil1329 <harshwardhanbpatil@gmail.com>
2024-08-27 10:30:16 +0530

    episode 1st

---
+++ app.js
@@ -0,0 +1,22 @@
+const parent =React.createElement("div",
+        {id:"parent"},
+        [
+        React.createElement("div",{id:"child1"},
+                [React.createElement("h1",{},"Nested H1 Tag"),
+                React.createElement("h2",{},"Nested H2 Tag")
+                ]),
+                React.createElement("div",{id:"child2"},
+               [React.createElement("h1",{},"Nested H1 Tag"),
+                React.createElement("h2",{},"Nested H2 Tag")
+                ])
+        ]
+)
+
+
+
+
+
+        console.log(parent);
+        
+        const root= ReactDOM.createRoot(document.getElementById("root"))
+        root.render(parent)
